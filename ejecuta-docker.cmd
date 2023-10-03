d8482dfb8850fe665452ed4787c8529e0ad75a5b
Cris Pedrani <crispedrani@gmail.com>
2023-10-02 21:12:19 -0300

    ejecutable docker windows correccion

--- ejecuta-docker.cmd
+++ ejecuta-docker.cmd
@@ -1,11 +1,11 @@
 # Construye la imagen
-podman build --tag humanos-conectados .
+docker build --tag humanos-conectados .
 
 # Elimina el contenedor si existe 
-podman container stop --time 5 humanos-conectados 
-podman container rm humanos-conectados 
+docker container stop --time 5 humanos-conectados 
+docker container rm humanos-conectados 
 
 # Crea un contenedor y lo ejecuta
-podman run -d -p 5000:5000 --name humanos-conectados localhost/humanos-conectados:latest
+docker run -d -p 5000:5000 --name humanos-conectados localhost/humanos-conectados:latest
 
 echo 'Ya está disponible http://127.0.0.1:5000'
